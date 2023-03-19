116c4475db552c4447e9dc86d68fdcd3f9908c47
Alemayehu Girma <alemayehu.girma@hotmail.com>
2023-03-19 21:45:31 +0100

    nextjs

--- pages/nextjs/index.tsx
+++ pages/nextjs/index.tsx
@@ -1,1 +1,25 @@
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
+
+export default function Pokemon() {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    async function getPokemon() {
+      const response = await fetch(
+        'https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json'
+      );
+      const data = await response.json();
+      setPokemon(data);
+    }
+    getPokemon();
+  }, []);
+  return (
+    <div>
+      <Head>
+        <title>Pokemon List</title>
+      </Head>
+      <div>{JSON.stringify(pokemon)}</div>
+    </div>
+  );
+}
